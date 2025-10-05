62b46cefe60c5d08d3286dfa2dc7ef476e56a47e
David Welber <welbersolucoesti@gmail.com>
2025-10-05 00:06:08 -0300

    remove MyRemoteService and its usage in WinthorAPI

--- src/main/java/br/dev/farmes/WinthorAPI.java
+++ src/main/java/br/dev/farmes/WinthorAPI.java
@@ -1,11 +1,5 @@
 package br.dev.farmes;
 
-import java.util.Set;
-
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-
-import br.dev.farmes.MyRemoteService.Extension;
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -14,21 +8,9 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/api")
 public class WinthorAPI {
 	
-	@Inject
-	@RestClient
-	MyRemoteService myRemoteService;
-	
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Bem-vindo ao Winthor API";
     }
-    
-    @GET
-    @Path("/status")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Set<Extension> quarkusStatus() {
-    	Set<MyRemoteService.Extension> restClientExtensions = myRemoteService.getExtensionsById("io.quarkus:quarkus-hibernate-validator");
-		return restClientExtensions;
-	}
 }
